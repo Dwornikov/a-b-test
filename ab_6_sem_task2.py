ed417eb73725accd265e0823439efaaf58fe5e7a
Dwornikov <den.dvornikov.88@inbox.ru>
2024-02-04 12:36:25 +0600

    Выполнил задание 2 из семинара 6

---
+++ ab_6_sem_task2.py
@@ -0,0 +1,24 @@
+import scipy.stats as stats
+
+# Заданные значения
+alpha = 0.05
+beta = 0.20
+mean_daily_traffic = 4000
+
+# Находим Z-значения для alpha/2 и beta
+z_alpha_half = stats.norm.ppf(1 - alpha / 2)
+z_beta = stats.norm.ppf(1 - beta)
+
+# Текущая и ожидаемая конверсии, минимально значимый эффект
+p1 = 0.40  # текущая bounce rate
+p2 = 0.40 * 0.8  # ожидаемая новая bounce rate после оптимизации (уменьшение на 20%)
+delta = p1 - p2  # минимально значимый эффект
+
+# Рассчитываем размер выборки
+n = ((z_alpha_half + z_beta) ** 2 * p1 * (1 - p1) + p2 * (1 - p2)) / delta**2
+
+# Рассчитываем длительность эксперимента в днях
+experiment_duration = n / mean_daily_traffic
+
+print(f"Размер выборки: {round(n)}")
+print(f"Длительность эксперимента (в днях): {round(experiment_duration)}")
