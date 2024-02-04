b4578eafe2668af73fbb1b4d311dbb2637115165
Dwornikov <den.dvornikov.88@inbox.ru>
2024-02-04 12:32:11 +0600

    Выполнил первое задание 6 семинара

---
+++ ab_6_sem.py
@@ -0,0 +1,22 @@
+import scipy.stats as stats
+
+# Заданные значения
+alpha = 0.05
+beta = 0.20
+sd = 156
+delta = 0.10  # относительное изменение
+mean_daily_traffic = 20000
+mean_time_spent = 25  # в минутах
+
+# Находим Z-значения для alpha/2 и beta
+z_alpha_half = stats.norm.ppf(1 - alpha / 2)
+z_beta = stats.norm.ppf(1 - beta)
+
+# Рассчитываем размер выборки
+n = ((z_alpha_half + z_beta) ** 2 * sd ** 2) / (delta ** 2 * mean_time_spent ** 2)
+
+# Рассчитываем длительность эксперимента в днях
+experiment_duration = n / mean_daily_traffic
+
+print(f"Размер выборки: {round(n)}")
+print(f"Длительность эксперимента (в днях): {round(experiment_duration)}")
